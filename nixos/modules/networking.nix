4633b4bbf500d3210bcc74451d2a8a41f20e5782
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-02 08:28:33 +0200

    removed ssh

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -4,10 +4,21 @@
   networking.hostName = "pc-maxlttr";
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = false;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = false;
+      AllowUsers = [ "maxlttr" "guest" ]; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = false;
+      X11Forwarding = false;
+      PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+      allowSFTP = true;
+    };
+  };  
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 80 443 8080 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
   networking.firewall.allowedUDPPorts = [ 53 ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
