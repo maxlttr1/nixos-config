159745bf6e2519fc660642e5cad1324babe36da2
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-03 20:12:01 +0200

    added module for auto upgrade

--- nixos/configurations/configuration-pc.nix
+++ nixos/configurations/configuration-pc.nix
@@ -4,14 +4,18 @@
   imports =
     [ # Include the results of the hardware scan.
       ../hardware-configuration.nix
+
       ../modules/apparmor.nix
+      ../modules/auto-upgrade.nix
       ../modules/cups.nix
       ../modules/default.nix
+      ../modules/experimental-features.nix
       ../modules/firewall.nix
       ../modules/flatpaks.nix
       ../modules/gamemode.nix
       ../modules/gnome.nix
       ../modules/grub.nix
+      ../modules/home.nix
       ../modules/intel.nix
       ../modules/kernel-latest.nix
       #../modules/nvidia.nix
@@ -26,21 +30,10 @@
       ../modules/timezone-locales.nix
       ../modules/tlp.nix
       ../modules/users.nix
-
     ];
 
   networking.hostName = "pc-maxlttr";
   
-  system.autoUpgrade = {
-    enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
-    dates = "17:00";
-  };
 # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -48,5 +41,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
---
+++ nixos/modules/auto-upgrade.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "17:00";
+  };
+}
