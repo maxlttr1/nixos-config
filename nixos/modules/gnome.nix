e1ffa31b96657e2893544e7db10416c1aac5bf21
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-04 19:38:23 +0200

    renable gnome dm

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -3,6 +3,6 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.gnome.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 }
