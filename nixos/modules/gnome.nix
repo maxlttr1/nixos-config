f399a7af335fbe7654a6cab50bab0b23160f21d5
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-05 11:14:30 +0200

    Update gnome.nix

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -3,6 +3,6 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 }
