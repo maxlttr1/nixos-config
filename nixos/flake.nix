ac2df56a04a8f977e90ccf53ad9faabd83930e3a
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 10:44:06 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -24,7 +24,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.maxlttr = import ./modules/home.nix;
+            #home-manager.users.maxlttr = import ./modules/home.nix;
           } 
           stylix.nixosModules.stylix
         ];
