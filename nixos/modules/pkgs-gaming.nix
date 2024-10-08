26063b6eb4408ff30bfa7dfb1f2ffc3f58b8fab1
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 19:25:30 +0200

    Update pkgs-gaming.nix

--- nixos/modules/pkgs-gaming.nix
+++ nixos/modules/pkgs-gaming.nix
@@ -4,7 +4,7 @@
   services.flatpak.packages = [
     "com.heroicgameslauncher.hgl"
     "net.lutris.Lutris"
-    "org.freedesktop.Platform.VulkanLayer.MangoHud"
+ #   "org.freedesktop.Platform.VulkanLayer.MangoHud"
     "com.moonlight_stream.Moonlight"
     "com.vysp3r.ProtonPlus"
     "org.libretro.RetroArch"
