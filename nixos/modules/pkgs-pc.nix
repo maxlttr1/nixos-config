5dc06e95354bdf9fe86d813271af34b899a580b0
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 11:24:23 +0200

    Update pkgs-pc.nix

--- nixos/modules/pkgs-pc.nix
+++ nixos/modules/pkgs-pc.nix
@@ -5,13 +5,19 @@
   services.flatpak.packages = [
     "com.jeffser.Alpaca"
     "co.logonoff.awakeonlan"
+    "org.kde.cantor"
     "com.bitwarden.desktop"
     "org.gnome.DejaDup"
     "com.discordapp.Discord"
+    "org.kde.filelight"
     "com.github.tchx84.Flatseal"
     "nl.hjdskes.gcolor3"
     "io.github.shiftey.Desktop"
-    "org.gnupg.GPA"
+    "org.kde.isoimagewriter"
+    "org.kde.kalgebra"
+    "org.kde.kdenlive"
+    "org.kde.kleopatra"
+    "org.kde.kontact"
     "org.libreoffice.LibreOffice"
     "io.gitlab.librewolf-community"
     "org.gnome.World.PikaBackup"
@@ -20,6 +26,7 @@
     "org.signal.Signal"
     "me.kozec.syncthingtk"
     "com.vscodium.codium"
+    "org.kde.yakuake"
   ];
 
 environment.systemPackages = with pkgs; [
