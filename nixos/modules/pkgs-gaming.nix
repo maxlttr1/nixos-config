b7b509201cb6e586588071fd0ca7b48647d4da6b
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 19:39:24 +0200

    Update pkgs-gaming.nix

--- nixos/modules/pkgs-gaming.nix
+++ nixos/modules/pkgs-gaming.nix
@@ -2,16 +2,19 @@
 
 {
   services.flatpak.packages = [
-    "com.heroicgameslauncher.hgl"
-    "net.lutris.Lutris"
+ #   "com.heroicgameslauncher.hgl"
+  #  "net.lutris.Lutris"
  #   "org.freedesktop.Platform.VulkanLayer.MangoHud"
-    "com.moonlight_stream.Moonlight"
+#    "com.moonlight_stream.Moonlight"
     "com.vysp3r.ProtonPlus"
-    "org.libretro.RetroArch"
-    "com.valvesoftware.Steam"
+    #"org.libretro.RetroArch"
+   # "com.valvesoftware.Steam"
   ];
  
+  programs.steam.enable = true;
+
   environment.systemPackages = with pkgs; [
+    lutris   
     vkbasalt
   ];
 }
