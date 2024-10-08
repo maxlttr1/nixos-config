dafb3fb3cdbffeb750a3f61ba10f6fa8e99f7cea
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 14:12:16 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,6 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
+
+    home-manager.url = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }  
   }; 
   
   outputs = { self, nixpkgs, nix-flatpak, ... }@inputs:{
