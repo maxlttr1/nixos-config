b65579990e846cd728d074226b6f92dd47bb6622
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 19:22:06 +0200

    Update pkgs-gaming.nix

--- nixos/modules/pkgs-gaming.nix
+++ nixos/modules/pkgs-gaming.nix
@@ -3,15 +3,15 @@
 {
   services.flatpak.packages = [
     "com.heroicgameslauncher.hgl"
+    "net.lutris.Lutris"
+    "org.freedesktop.Platform.VulkanLayer.MangoHud"
     "com.moonlight_stream.Moonlight"
+    "com.vysp3r.ProtonPlus"
     "org.libretro.RetroArch"
     "com.valvesoftware.Steam"
   ];
  
   environment.systemPackages = with pkgs; [
-    mangohud
-    protonup-qt
-    unzip
     vkbasalt
   ];
 }
