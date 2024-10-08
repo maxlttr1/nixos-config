f4baa5e0dcabb02728bfaffe02b4f7f7f7b302b7
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 14:19:23 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -22,7 +22,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.jdoe = import ./home.nix;
+            home-manager.users.jdoe = import ./modules/home.nix;
           }
         ];
       };
