d30ee58d0539f2825bf5d0ae0ea7921092728a8a
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 18:17:16 +0200

    Update firewall.nix

--- nixos/modules/firewall.nix
+++ nixos/modules/firewall.nix
@@ -2,7 +2,7 @@
   # Open ports in the firewall.
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 80 443 ]
+    allowedTCPPorts = [ 80 443 ];
     allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
     allowedUDPPorts = [ 53 ];
     allowedUDPPortRanges = allowedTCPPortRanges;
