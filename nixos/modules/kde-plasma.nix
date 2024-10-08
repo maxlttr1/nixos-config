4afac666bcfc9e9a5b31a853900f4e17ba26cd45
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 10:56:40 +0200

    Update kde-plasma.nix

--- nixos/modules/kde-plasma.nix
+++ nixos/modules/kde-plasma.nix
@@ -2,10 +2,4 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
-  };
 }
