b198a06b7f0b7728ad0e91defd3f0014cae5743e
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 11:08:56 +0200

    Update kde-plasma.nix

--- nixos/modules/kde-plasma.nix
+++ nixos/modules/kde-plasma.nix
@@ -2,4 +2,6 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+  services.displayManager.defaultSession = "plasma";
+  services.displayManager.sddm.wayland.enable = true;
 }
