9adb886a0fe423ceb0c1c14035737fcbf4f5d830
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 10:47:21 +0200

    updaed flakes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,28 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    stylix.url = "github:danth/stylix";
   }; 
   
-  outputs = { self, nixpkgs, home-manager, stylix, nix-flatpak, ... }@inputs:{
+  outputs = { self, nixpkgs, nix-flatpak, ... }@inputs:{
       nixosConfigurations."pc-maxlttr" = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs; };
         modules = [ 
           ./configuration.nix
           nix-flatpak.nixosModules.nix-flatpak
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            #home-manager.users.maxlttr = import ./modules/home.nix;
-          } 
-          stylix.nixosModules.stylix
         ];
       };
   };
