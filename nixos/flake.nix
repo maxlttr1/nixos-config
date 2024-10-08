2f56a5188de0acfc063bb3ead5a33124891a8944
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-08 14:50:47 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -12,17 +12,33 @@
     };  
   }; 
   
-  outputs = { self, nixpkgs, home-manager, nix-flatpak, ... }@inputs:{
+  outputs = @inputs{ self, nixpkgs, home-manager, nix-flatpak, ... }:
+    let
+      username = "maxlttr";
+
+      # Replace with the fitting architecture
+      system = "x86_64-linux";
+    in
+    {
       nixosConfigurations."pc-maxlttr" = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs; };
         modules = [ 
           ./configuration.nix
+          {
+            system.stateVersion = "24.05";
+            users.users."${username}".isNormalUser = true;
+            networking.hostName = "pc-maxlttr";
+            fileSystems."/".device = "/dev/sda";  # Root filesystem
+            boot.loader.grub.enable = true;        # Enable GRUB
+            boot.loader.grub.devices = [ "/dev/sda" ];  # Device for GRUB installation
+            boot.loader.grub.useOSProber = true;   # Enable OS detection
+          }
           nix-flatpak.nixosModules.nix-flatpak
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.maxlttr = import ./modules/home.nix;
+            home-manager.users."$(username)" = import ./modules/home.nix;
             home-manager.backupFileExtension= "backup";
           }
         ];
