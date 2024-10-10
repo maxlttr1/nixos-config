bb47f06d27c271e5cfab91623c15952c0e4968e6
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-10 20:36:02 +0200

    Create grub.nix

---
+++ nixos/modules/common/grub.nix
@@ -0,0 +1,6 @@
+{
+  boot.loader.grub.enable = true;        # Enable GRUB
+  boot.loader.grub.devices = [ "${grub-disk}" ];  # Device for GRUB in>
+  boot.loader.grub.useOSProber = true;   # Enable OS detection
+  boot.loader.grub.configurationLimit = 10;
+}
