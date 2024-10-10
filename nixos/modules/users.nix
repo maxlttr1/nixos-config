6e92a61563480383115bcc189a24d1818c82164f
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-10 20:08:35 +0200

    Update users.nix

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -7,7 +7,7 @@
       isNormalUser = true;
       createHome = true;
       extraGroups = [ "networkmanager" "wheel" ];
-      initialhashedPassword = "$y$j9T$FNklRaiyPvHgR8ueH12z2/$q8k1QXd/CIbwQqV8rdEzGeAOVVL3N9cBa1pP4zyz0m8";
+      initialHashedPassword = "$y$j9T$FNklRaiyPvHgR8ueH12z2/$q8k1QXd/CIbwQqV8rdEzGeAOVVL3N9cBa1pP4zyz0m8";
       packages = with pkgs; [
       ];
     };
