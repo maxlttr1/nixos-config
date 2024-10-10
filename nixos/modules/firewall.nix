ab10a469a5a8e0aa5d80c3a8b0a29eda7c22da24
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-10 20:21:10 +0200

    Update firewall.nix

--- nixos/modules/firewall.nix
+++ nixos/modules/firewall.nix
@@ -3,8 +3,6 @@
   networking.firewall = rec {
     enable = true;
     allowedTCPPorts = [ 80 443 ];
-    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
     allowedUDPPorts = [ 53 ];
-    allowedUDPPortRanges = allowedTCPPortRanges;
   };
 }
