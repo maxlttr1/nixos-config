66168eef3d1edb1ee7b71eb4be64e6b48d3912b3
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-11 12:17:54 +0200

    Update intel.nix

--- nixos/modules/intel.nix
+++ nixos/modules/intel.nix
@@ -4,5 +4,8 @@
  
   # Drivers
   services.xserver.videoDrivers = [ "modesetting" ]; # Intel
+
+  # Thermald proactively prevents overheating on Intel CPUs and works well with other tools
+  services.thermald.enable = true;
 }
 
