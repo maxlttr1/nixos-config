3b1cfa77d6c3aab70c5282be078f09691f19e22d
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-17 20:24:31 +0200

    Update bootloader.nix

--- nixos/modules/common/bootloader.nix
+++ nixos/modules/common/bootloader.nix
@@ -6,8 +6,9 @@
       grub = {
         efiSupport = true;
         device = "nodev"; #GRUB not tied to a specific physical disk
+      };
     } else {
-    grub.device = "${grub-disk}";
+      grub.device = "${grub-disk}";
     }
     { 
       grub = {
