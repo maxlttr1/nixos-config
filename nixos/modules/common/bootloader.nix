4b2ad25e607806b3d97dca4b9331b52bf5357162
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-17 20:02:18 +0200

    Update bootloader.nix

--- nixos/modules/common/bootloader.nix
+++ nixos/modules/common/bootloader.nix
@@ -3,7 +3,10 @@
 {
   boot.loader = if (builtins.pathExists "/sys/firmware/efi") then {
     # UEFI mode
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    }
     efi.canTouchEfiVariables = true;
   } else {
     # BIOS mode
