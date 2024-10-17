c90a4d33ba0a38c78bee303762e101b735d9af66
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-17 19:48:54 +0200

    Create bootloader.nix

---
+++ nixos/modules/common/bootloader.nix
@@ -0,0 +1,13 @@
+{ grub-disk, ... }:
+
+{
+  boot.loader = if (builtins.pathExists "/sys/firmware/efi") then {
+    # UEFI mode
+    systemd-boot.enable = true;
+    systemd-boot.efiSupports = true;
+  } else {
+    # BIOS mode
+    grub.enable = true;
+    grub.device = "${grub-disk}";
+  };
+
