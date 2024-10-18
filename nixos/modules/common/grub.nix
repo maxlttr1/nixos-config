271d6aeafaad7ce4b7fe40c0dd24b1f51ceaf6be
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-18 07:53:33 +0200

    rmeoved unecessar yfiles

--- nixos/modules/common/grub.nix
+++
@@ -1,8 +0,0 @@
-{ grub-disk, ... }:
-
-{
-  boot.loader.grub.enable = true;        # Enable GRUB
-  boot.loader.grub.devices = [ "${grub-disk}" ];  # Device for GRUB in>
-  boot.loader.grub.useOSProber = true;   # Enable OS detection
-  boot.loader.grub.configurationLimit = 10;
-}
