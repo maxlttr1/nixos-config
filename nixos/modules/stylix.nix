ec4ab92556ebec50183fdb4b0c329ee5248854c7
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-22 08:31:59 +0200

    added bibata cursor in stylix

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -1,6 +1,13 @@
+{ pkgs, ... }:
+
 {
   stylix = { 
     enable = true;
     image = ../wallpaper.jpg;
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = “Bibata-Modern-Ice”;
+      size = 25;
+    };
   };
 }
