4393166f053a315661669b32b46a09d063a7502b
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-22 14:55:45 +0200

    dndndndn

--- nixos/modules/pkgs.nix
+++ nixos/modules/pkgs.nix
@@ -26,6 +26,10 @@
   ];
 
   environment.systemPackages = with pkgs; [
+    (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
+      [General]
+      background=${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/MilkyWay/contents/images/5120x2880.png
+    '')
     bibata-cursors
     curl
     fastfetch
