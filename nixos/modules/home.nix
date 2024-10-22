020795251b35a7ea337194bf9ab07adb12533cde
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-22 10:54:43 +0200

    dddd

--- nixos/modules/home.nix
+++ nixos/modules/home.nix
@@ -23,7 +23,7 @@
      panels = [
       # Windows-like panel at the bottom
       {
-        location = [ "floating" ];
+        location = "floating";
         widgets = [
           {
             name = "org.kde.plasma.kickoff";
