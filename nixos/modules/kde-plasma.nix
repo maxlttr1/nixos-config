ac704081428503b49813ec69f532dc61db2774b8
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-22 16:31:02 +0200

    replaced sddm by gdm

--- nixos/modules/kde-plasma.nix
+++ nixos/modules/kde-plasma.nix
@@ -1,9 +1,14 @@
 {
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
+  
   services.displayManager.defaultSession = "plasma";
-  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  #services.displayManager.sddm.wayland.enable = true;
+  #services.displayManager.sddm.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
 
   programs.kdeconnect.enable = true;
 }
