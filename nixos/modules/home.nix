d718b80cef4fdcf114d60ec2d7b27984b9965c23
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-22 09:24:54 +0200

    cursors size to 32

--- nixos/modules/home.nix
+++ nixos/modules/home.nix
@@ -16,7 +16,7 @@
       clickItemTo = "open"; # If you liked the click-to-open default from plasma 5
       cursor = {
         theme = "Bibata-Modern-Ice";
-        size = 25;
+        size = 32;
       };
       iconTheme = "Papirus-Dark";
     };
