4ff39eed3a41495265f4a3799fc07f60492c3235
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-24 12:21:09 +0200

    ddd

--- flake.nix
+++ flake.nix
@@ -24,44 +24,33 @@
   }; 
   
   outputs = inputs@{ self, nixpkgs, ... }:
-    let
-      settings = {
-        username = "maxlttr";
-        hostname = "pc-maxlttr";
-        system = "x86_64-linux";
-        kernel = "linuxPackages";
-      };
-    in
     {
-      nixosConfigurations."${settings.hostname}" = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs settings; };
-        modules = [
-          ./modules/apparmor.nix
-          ./modules/cups.nix
-          ./modules/gamemode.nix
-          ./modules/intel.nix
-          ./modules/kde-plasma.nix
-          ./modules/pipewire.nix
-          ./modules/pkgs.nix
-          ./modules/podman.nix
-          ./modules/ssh.nix
-          ./modules/tailscale.nix
-          ./modules/tlp.nix
-          ./modules/touchpad.nix
-          ./modules/virt-manager.nix
-          ./modules/common
-          ./disko.nix
-          inputs.disko.nixosModules.disko
-          inputs.nix-flatpak.nixosModules.nix-flatpak
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users."${settings.username}" = import ./modules/home.nix;
-            home-manager.sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
-            home-manager.backupFileExtension= "backup";
-          }
-        ];
+      nixosConfigurations = {
+        thinkpad = 
+          let
+            settings = {
+              username = "maxlttr";
+              hostname = "thinkpad-maxlttr";
+              system = "x86_64-linux";
+              kernel = "linuxPackages";
+            };
+            specialArgs = { inherit inputs settings; };
+          in
+            nixpkgs.lib.nixosSystem {
+              modules = [
+                ./hosts/thinkpad
+                inputs.disko.nixosModules.disko
+                inputs.nix-flatpak.nixosModules.nix-flatpak
+                inputs.home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.users."${settings.username}" = import ./modules/home.nix;
+                  home-manager.sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
+                  home-manager.backupFileExtension= "backup";
+                }
+              ];
+            };
       };
-  };
+    };
 }
