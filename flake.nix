e32ac43931763b26f972e928d670e1bed4d386dd
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-24 20:20:49 +0200

    rfg,ek

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
             #pkgs = nixpkgs.legacyPackages.${settings.system};
           in
             nixpkgs.lib.nixosSystem {
-              specialArgs = { inherit inputs setting; }; #pkgs; };
+              specialArgs = { inherit inputs settings; }; #pkgs; };
               modules = [
                 ./hosts/thinkpad
                 inputs.disko.nixosModules.disko
