a81492830bf32ce7f726f0c79e2384c04562394b
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-24 11:41:00 +0200

    created new dir for hosts

---
+++ hosts/thinkpad/default.nix
@@ -0,0 +1,20 @@
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./disko.nix
+    ../../modules/apparmor.nix
+    ../../modules/cups.nix
+    ../../modules/gamemode.nix
+    ../../modules/intel.nix
+    ../../modules/kde-plasma.nix
+    ../../modules/pipewire.nixs
+    ../../modules/pkgs.nix
+    ../../modules/podman.nix
+    ../../modules/ssh.nix
+    ../../modules/tailscale.nix
+    ../../modules/tlp.nix
+    ../../modules/touchpad.nix
+    ../../modules/virt-manager.nix
+    ../../modules/common
+  ]
+}
---
+++ hosts/thinkpad/disko.nix
@@ -0,0 +1,45 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            plainSwap = {
+              size = "16G";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
