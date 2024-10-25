096f9e9418ec2c09dd9df5384c38fb855f27e88f
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-25 12:22:24 +0200

    added asus to flake.nix

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, ... }:
     {
       nixosConfigurations = {
-        thinkpad = 
+        thinkpad-maxlttr = 
           let
             settings = {
               username = "maxlttr";
@@ -61,7 +61,40 @@
                 }
               ];
             };
-        server = 
+        asus-maxlttr = 
+          let
+            settings = {
+              username = "maxlttr";
+              hostname = "asus-maxlttr";
+              system = "x86_64-linux";
+              kernel = "linuxPackages_latest";
+            };
+            overlay-unstable = final: prev: {
+              unstable = import nixpkgs-unstable {
+                system = settings.system;
+                config.allowUnfree = true;
+              };
+            };
+          in
+            nixpkgs.lib.nixosSystem {
+              system = settings.system;
+              specialArgs = { inherit inputs settings; }; #pkgs; };
+              modules = [
+                ./hosts/asus
+                ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+                inputs.disko.nixosModules.disko
+                inputs.nix-flatpak.nixosModules.nix-flatpak
+                inputs.home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.users."${settings.username}" = import ./modules/home.nix;
+                  home-manager.sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
+                  home-manager.backupFileExtension= "backup";
+                }
+              ];
+            };
+        server-maxlttr = 
           let
             settings = {
               username = "maxlttr";
