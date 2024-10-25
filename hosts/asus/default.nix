9d60cfc389a4da364660c6502a2a1a931e65aa39
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-25 12:16:16 +0200

    added asus

---
+++ hosts/asus/default.nix
@@ -0,0 +1,21 @@
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./disko.nix
+    ../../modules/apparmor.nix
+    ../../modules/cups.nix
+    ../../modules/gamemode.nix
+    ../../modules/intel.nix
+    ../../modules/kde-plasma.nix
+    ../../modules/ollama.nix
+    ../../modules/pipewire.nix
+    ../../modules/pkgs.nix
+    ../../modules/podman.nix
+    ../../modules/ssh.nix
+    ../../modules/tailscale.nix
+    ../../modules/tlp.nix
+    ../../modules/touchpad.nix
+    ../../modules/virt-manager.nix
+    ../../modules/common
+  ];
+}
