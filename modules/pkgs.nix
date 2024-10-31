6bb50f77a369448f5a85137b33151b807b40eb48
maxlttr1 <maxime.lettier@protonmail.com>
2024-10-31 19:42:14 +0100

    jkhdjkikj

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -5,6 +5,7 @@
   services.flatpak.packages = [
     "co.logonoff.awakeonlan"
     "com.usebottles.bottles"
+    "com.gitlab.davem.ClamTk"
     "com.discordapp.Discord"
     "org.mozilla.firefox"
     "com.github.tchx84.Flatseal"
