ca1beaba7929cbf64081ef33d67b01cac3d7cd9e
maxlttr1 <maxime.lettier@protonmail.com>
2024-11-02 21:34:26 +0100

    added moonlight and removed zen

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -4,13 +4,13 @@
   # Flatpaks
   services.flatpak.packages = [
     "co.logonoff.awakeonlan"
-    "com.usebottles.bottles"
     "com.discordapp.Discord"
     "com.github.tchx84.Flatseal"
+    "io.gitlab.librewolf-community"
+    "com.moonlight_stream.Moonlight"
     "com.protonvpn.www"
     "com.visualstudio.code"
     "org.kde.yakuake"
-    "io.github.zen_browser.zen"
   ];
 
   environment.systemPackages = 
