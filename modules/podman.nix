29e355ca366cf52f8a540b0c81b1ce0103f5487d
maxlttr1 <maxime.lettier@protonmail.com>
2024-11-13 11:37:26 +0100

    test for conatianers

--- modules/podman.nix
+++ modules/podman.nix
@@ -6,6 +6,11 @@
   virtualisation = {
     podman = {
       enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      
+      # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true; 
     };
   };
@@ -16,5 +21,14 @@
     podman-compose #start group of containers for dev
     podman-tui #status of the containers in the terminal
   ];
+
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.oci-containers.containers = {
+    container-name = {
+      image = "hello-world";
+      autoStart = true;
+      #ports = [ "127.0.0.1:1234:1234" ];
+    };
+  };
 
 }
