db293434d219f811cd1024a07c7ea6dc765f49fa
maxlttr1 <maxime.lettier@protonmail.com>
2024-11-13 14:16:03 +0100

    open ports for syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,1 +1,5 @@
-{services.syncthing.enable = true;}
+{
+  services.syncthing.enable = true;
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+}
