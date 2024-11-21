54e27420b9cd81e810ded83ad7f864c48bc89ad1
maxlttr1 <maxime.lettier@protonmail.com>
2024-11-21 20:33:23 +0100

    removed bloat syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,13 +1,8 @@
-{ settings, ... }:
-
 {
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
     overrideDevices = true;     # overrides any devices added or deleted through the WebUI
     overrideFolders = true;     # overrides any folders added or deleted through the WebUI
-    #user = "${settings.username}";
   };
-  /*networking.firewall.allowedTCPPorts = [ 8384 22000 ];
-  networking.firewall.allowedUDPPorts = [ 22000 21027 ];*/
 }
