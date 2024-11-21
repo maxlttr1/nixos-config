c2c02c6f885903b6e547a36486c598c983141a71
maxlttr1 <maxime.lettier@protonmail.com>
2024-11-21 20:32:33 +0100

    removed user for syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -6,7 +6,7 @@
     openDefaultPorts = true;
     overrideDevices = true;     # overrides any devices added or deleted through the WebUI
     overrideFolders = true;     # overrides any folders added or deleted through the WebUI
-    user = "${settings.username}";
+    #user = "${settings.username}";
   };
   /*networking.firewall.allowedTCPPorts = [ 8384 22000 ];
   networking.firewall.allowedUDPPorts = [ 22000 21027 ];*/
