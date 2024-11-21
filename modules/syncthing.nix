4b670913ac9530e7ebf4b6c2e1ddf40cd8a4b477
maxlttr1 <maxime.lettier@protonmail.com>
2024-11-21 20:47:39 +0100

    test user for syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,8 +1,11 @@
+{ settings, ... }:
+
 {
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
     overrideDevices = true;     # overrides any devices added or deleted through the WebUI
     overrideFolders = true;     # overrides any folders added or deleted through the WebUI
+    user = "${settings.username}";
   };
 }
