34348d8538b77897618a6c7c204481386442ffd0
maxlttr1 <maxime.lettier@protonmail.com>
2024-11-21 20:06:03 +0100

    added default folder for syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,13 +1,13 @@
+{ settings, ... }:
+
 {
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
-    /*settings.gui = {
-      user = "maxlttr";
-      password = "passwd";
-    };k*/
+    overrideDevices = true;     # overrides any devices added or deleted through the WebUI
+    overrideFolders = true;     # overrides any folders added or deleted through the WebUI
+    dataDir = "/home/${settings.username}/Syncthing";
   };
-
   /*networking.firewall.allowedTCPPorts = [ 8384 22000 ];
   networking.firewall.allowedUDPPorts = [ 22000 21027 ];*/
 }
