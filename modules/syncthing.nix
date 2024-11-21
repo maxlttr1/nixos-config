ef048b9f14d9f3a9859f3a39435db5a173cc65bf
maxlttr1 <maxime.lettier@protonmail.com>
2024-11-21 20:18:31 +0100

    added and group and tweaked synthing config

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -6,7 +6,7 @@
     openDefaultPorts = true;
     overrideDevices = true;     # overrides any devices added or deleted through the WebUI
     overrideFolders = true;     # overrides any folders added or deleted through the WebUI
-    dataDir = "/home/${settings.username}/Syncthing";
+    #dataDir = "/home/${settings.username}/Syncthing";
   };
   /*networking.firewall.allowedTCPPorts = [ 8384 22000 ];
   networking.firewall.allowedUDPPorts = [ 22000 21027 ];*/
