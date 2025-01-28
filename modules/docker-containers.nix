8302015c482858bd72b2c133274e3cae786fd336
Maxime <E226620B@etu.univ-nantes.fr>
2025-01-28 14:34:59 +0100

    modified docker config

--- modules/docker-containers.nix
+++ modules/docker-containers.nix
@@ -7,49 +7,26 @@
     backend = "docker";
     containers = {
       jellyfin = {
-        autoStart = true;
+        autoStart = false;
         hostname = "jellyfin";
         image = "lscr.io/linuxserver/jellyfin:latest";
         environment = {
           PUID="1000";
           PGID="1000";
           TZ="Etc/UTC";
-          #NVIDIA_VISIBLE_DEVICES="all";
         };
         volumes = [
           "/home/maxlttr/.docker/jellyfin/library:/config"
-          "/home/maxlttr/Videos:/data/tvshows"
-          "/home/maxlttr/Videos:/data/movies"
+          #"/home/maxlttr/Syncthing/:/data/tvshows"
+          "/home/maxlttr/Syncthing/movies:/data/movies"
         ];
         ports = [
           "8096:8096"
         ];
         cmd = [
-          "--runtime=nvidia"
-          "--device=nvidia.com/gpu=all"
-        ];
-      };
-
-      open-webui = {
-        hostname = "open-webui";
-        image = "ghcr.io/open-webui/open-webui:ollama";
-        cmd = [
-            #"--gpus=all"
-            "--device=nvidia.com/gpu=all"
-            "--add-host=host.docker.internal:host-gateway"
-        ];
-        volumes = [
-          "/home/maxlttr/.docker/open-webui:/app/backend/data"
-          "/home/maxlttr/.docker/ollama:/root/.ollama"
-        ];
-        ports = [
-          "3000:8080"
+          "--device=/dev/dri:/dev/dri"
         ];
       };
     };
   };
-
-  /*environment.systemPackages = [
-    pkgs.nvidia-container-toolkit
-  ];*/
 }
