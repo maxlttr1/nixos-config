bbf7c952278c245f3778055dfc553eb427e1a933
Maxime <E226620B@etu.univ-nantes.fr>
2025-01-28 15:13:35 +0100

    added windows docker

--- modules/docker-containers.nix
+++ modules/docker-containers.nix
@@ -40,6 +40,28 @@
           "portainer_data:/data"
         ];
       };
+      windows = {
+        hostname = "windows";
+        autoStart = false;
+        image = "dockurr/windows";
+        environment = {
+          #VERSION = "11";
+          VERSION = "https://example.com/win.iso";
+          DISK_SIZE = "64G";
+          RAM_SIZE =  "8G";
+          CPU_CORES = "4";
+        ports = [
+          "8006:8006"
+          "3389:3389/tcp"
+          "3389:3389/udp"
+        ];
+        cmd = [
+          "--device=/dev/kvm"
+          "--device=/dev/net/tun"
+          "--cap-add=NET_ADMIN"
+        ];
+        }
+      };
     };
   };
 }
