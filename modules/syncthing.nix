5eb567bcd4b69f6097c00245dc9de7d4800686dc
Maxime <E226620B@etu.univ-nantes.fr>
2025-01-28 13:49:02 +0100

    added syncthing id config

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -10,5 +10,10 @@
     group = "syncthing";
     dataDir = "/home/${settings.username}/.syncthing";   # Custom data directory for Syncthing
     configDir = "/home/${settings.username}/.config/syncthing";  # Custom config directory
+    settings = {
+      devices = {
+        "SM-A52" = { id = "542EBS7-3JPT3VG-KLZG73F-BOJJQTB-SHUR2OF-UKZWKC4-JZ5RM2R-5RHPXA2"; };
+      };
+    };
   };
 }
