e1fe7497fc254135064d114160f44c08f93cf0b4
Maxime <E226620B@etu.univ-nantes.fr>
2025-01-29 18:07:41 +0100

    added extensions

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -56,23 +56,32 @@
       # "force_installed" and "normal_installed".
       ExtensionSettings = {
         "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        
         # uBlock Origin:
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
         };
-        # Privacy Badger:
-        "jid1-MnnxcxisBPnSXQ@jetpack" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+        
+        # Unhook (YouTube Disabler):
+        "unhook@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/unhook/latest.xpi";
           installation_mode = "force_installed";
         };
-        # 1Password:
-        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+
+        # Proton Pass (Password Manager):
+        "protonpass@protonmail.com" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+          installation_mode = "force_installed";
+        };
+
+        # SponsorBlock (Skip YouTube sponsor segments):
+        "sponsorblock@sponsor.rix0r" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
           installation_mode = "force_installed";
         };
       };
-  
+
       /* ---- PREFERENCES ---- */
       # Check about:config for options.
       Preferences = { 
