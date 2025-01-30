7922a6afc5f15915e226e0b02c6fd001faeb2cfe
Maxime Lettier <maxime.lettier@etu.univ-nantes.fr>
2025-01-30 14:49:00 +0100

    finalized firefox config

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -55,6 +55,16 @@ in
 
     preferences = {
       "widget.use-xdg-desktop-portal.file-picker" = 1; # Use the file picker offered by the XDG Desktop Portal framework
+      "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+      "browser.contentblocking.fingerprinting.preferences.ui.enabled" = lock-true;
+      "browser.contentblocking.reject-and-isolate-cookies.preferences.ui.enabled" = lock-true;
+      "browser.topsites.contile.enabled" = lock-false; # Disables the Top Sites feature, meaning users won’t see suggested sites based on their browsing habits on the New Tab Page
+      "media.hardware-video-decoding.enabled" = lock-true;
+      "network.trr.disable-ECS" = lock-true;
+      "privacy.firstparty.isolate" = lock-true; # Isolates cookies and storage for each site, improving privacy by preventing cross-site tracking
+      "privacy.donottrackheader.enabled" = lock-true; # Sends a Do Not Track request in the HTTP header
+      "privacy.resistFingerprinting" = lock-true; # Reduces your device's unique signature (blocks certain data like screen resolution, time zone, etc.)
+      "privacy.trackingprotection.socialtracking.enabled" = lock-true; # locks social media tracking, including cookies and scripts used by social media platforms to track users across different websites
     };
 
     languagePacks = [ "en-US" ];
@@ -84,17 +94,27 @@ in
       "DisablePrivateBrowsing" = true;
       "DisableProfileImport" = true;
       "DisableTelemetry" = true;
-
-
-
-
-
+      "DNSOverHTTPS" = {
+        "Enabled" =  true;
+        "ProviderURL" = "https://mozilla.cloudflare-dns.com/dns-query";
+        "Locked" = true;
+        #"ExcludedDomains" = ["example.com"],
+        "Fallback" = true;
+      };
+      "EnableTrackingProtection" = {
+        "Value" = true;
+        "Locked" = true;
+        "Cryptomining" = true;
+        "Fingerprinting" = true;
+        "EmailTracking" = true;
+        #"Exceptions": ["https://example.com"]
+      };
       # Check about:support for extension/add-on ID strings.
       ExtensionSettings = {
         "*".installation_mode = "blocked"; # blocks all addons except the ones specified below        
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "normal_installed";
+          installation_mode = "force_installed";
         };
         "unhook@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/unhook/latest.xpi";
@@ -102,43 +122,73 @@ in
         };
         "protonpass@protonmail.com" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
-          installation_mode = "normal_installed";
+          installation_mode = "force_installed";
         };
         "sponsorblock@sponsor.rix0r" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
-          installation_mode = "normal_installed";
+          installation_mode = "force_installed";
         };
       };
-
-      Preferences = { 
-        "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-        "browser.contentblocking.fingerprinting.preferences.ui.enabled" = lock-true;
-        "browser.contentblocking.reject-and-isolate-cookies.preferences.ui.enabled" = lock-true;
-        "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
-        "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
-        "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
-        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
-        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
-        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
-        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
-        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
-        "browser.search.suggest.enabled" = lock-false;
-        "browser.search.suggest.enabled.private" = lock-false;
-        "browser.topsites.contile.enabled" = lock-false; # Disables the Top Sites feature, meaning users won’t see suggested sites based on their browsing habits on the New Tab Page
-        "browser.translations.neverTranslateLanguages" = [ "en" "fr" ];
-        "media.hardware-video-decoding.enabled" = lock-true;
-        "network.trr.disable-ECS" = lock-true;
-        "network.trr.uri" = "https://mozilla.cloudflare-dns.com/dns-query";
-        "network.trr.mode" = 2; # Enable DNS over HTTPS (DoH) for all queries
-        "privacy.firstparty.isolate" = lock-true; # Isolates cookies and storage for each site, improving privacy by preventing cross-site tracking
-        "privacy.donottrackheader.enabled" = lock-true; # Sends a Do Not Track request in the HTTP header
-        "privacy.resistFingerprinting" = lock-true; # Reduces your device's unique signature (blocks certain data like screen resolution, time zone, etc.)
-        "privacy.trackingprotection.enabled"= lock-true;
-        "privacy.trackingprotection.cryptomining.enabled" = lock-true; # Blocks cryptomining scripts 
-        "privacy.trackingprotection.pbmode.enabled" = lock-true; # Enables tracking protection in Private Browsing mode
-        "privacy.trackingprotection.socialtracking.enabled" = lock-true; # locks social media tracking, including cookies and scripts used by social media platforms to track users across different websites
-        "signon.rememberSignons" = lock-false; 
+      "ExtensionUpdate" = true;
+      "FirefoxHome" = {
+        "Search" = false;
+        "TopSites" = false;
+        "SponsoredTopSites" = false;
+        "Highlights" = false;
+        "Pocket" = false;
+        "SponsoredPocket" = false;
+        "Snippets" = false;
+        "Locked" = true;
+      };
+      "FirefoxSuggest" = {
+        "WebSuggestions" = false;
+        "SponsoredSuggestions" = false;
+        "ImproveSuggest" = false;
+        "Locked" = true;
+      };
+      "HardwareAcceleration" = true; # Preferences Affected: layers.acceleration.disabled
+      "Homepage"= {
+        #"URL"= "http://example.com/";
+        "Locked" = true;
+        /*"Additional": ["http://example.org/",
+                     "http://example.edu/"],*/
+        "StartPage" = "previous-session";
+      };
+      "HttpsOnlyMode" = "enabled";
+      "ManagedBookmarks" = [
+        {
+          "name" = "My favs";
+          "children" = [
+            {
+              "url" = "youtube.com";
+              "name" = "Youtube";
+            }
+          ];
+        }
+      ];
+      "NewTabPage" = false;
+      "NoDefaultBookmarks" = true;
+      "OfferToSaveLoginsDefault" = true;
+      "OverrideFirstRunPage" = ""; # If the value is an empty string (“”), the first run page is not displayed
+      "PasswordManagerEnabled" = false;
+      "PostQuantumKeyAgreementEnabled" = true;
+      "PrivateBrowsingModeAvailability" = 1; # 0 (Private Browsing mode is available), 1 (Private Browsing mode not available), and 2(Private Browsing mode is forced)
+      "PromptForDownloadLocation" = true;
+      "SanitizeOnShutdown" = true; # All !
+      "SearchEngines" = {
+        "Default" = "DuckDuckGo";
+        "PreventInstalls" = true; 
+      };
+      "SearchSuggestEnabled" = false;
+      "TranslateEnabled" = false;
+      "UserMessaging" = {
+        "ExtensionRecommendations" = false;
+        "FeatureRecommendations" = false;
+        "UrlbarInterventions" = false;
+        "SkipOnboarding" = false;
+        "MoreFromMozilla" = false;
+        "FirefoxLabs" = false;
+        "Locked" = true;
       };
     };
   };
