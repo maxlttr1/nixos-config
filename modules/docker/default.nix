83028f9a1ff95b009c48864da8162f12f1dfe15d
Maxime <maxime.lettier@protonmail.com>
2025-02-04 09:24:12 +0100

    added jellyfin

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -2,7 +2,7 @@
  imports = [
    ./docker.nix
    #./dockur_output.nix
-   #./jellyfin_output.nix
+   ./jellyfin_output.nix
    #./nginx_reverse_proxy_output.nix
    #./pi-hole_output.nix
    #./portainer_output.nix
