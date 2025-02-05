53f92cd7e9725f85ce0537bc53173deb299b7ec5
Maxime <maxime.lettier@protonmail.com>
2025-02-05 08:19:14 +0100

    open port 22 by default

--- modules/common/firewall.nix
+++ modules/common/firewall.nix
@@ -2,7 +2,7 @@
   # Open ports in the firewall.
   networking.firewall = rec {
     enable = true;
-    allowedTCPPorts = [ 80 443 ];
+    allowedTCPPorts = [ 22 80 443 ];
     allowedUDPPorts = [ 53 ];
   };
 }
