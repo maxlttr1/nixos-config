db8eea3316cfe63f8cf16754edc93d8d677eea85
Maxime <maxime.lettier@protonmail.com>
2025-02-05 12:31:57 +0100

    test

--- packages/sddm-theme.nix
+++ packages/sddm-theme.nix
@@ -1,8 +1,8 @@
-{ pkgs, fetchFromGitHub }:
+{ pkgs }:
 
 pkgs.stdenv.mkDerivation {
   name = "sddm-theme";
-  src = fetchFromGitHub {
+  src = pkgs.fetchFromGitHub {
     owner = "MarianArlt";
     repo = "sddm-sugar-dark";
     rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
