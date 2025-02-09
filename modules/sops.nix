95e783f111f8a956eaef759c1944ad5050597459
Maxime <maxime.lettier@protonmail.com>
2025-02-09 19:16:38 +0100

    test

--- modules/sops.nix
+++ modules/sops.nix
@@ -5,18 +5,14 @@
 
     sops.defaultSopsFile = ../secrets/secrets.yaml;
     sops.defaultSopsFormat = "yaml";
-    sops.age.keyFile = "/etc/sops/keys.txt";
+    sops.age.keyFile = "~/.config/sops/age/keys.txt";
 
     sops.secrets.passwd = {
-        #neededForUsers = true;
         owner = "${settings.username}";
         #config.users.users.maxlttr.name;
     };
 
     sops.secrets."vpn.env" = {
         owner = "${settings.username}"; 
-        #config.users.users.maxlttr.name;
-        #path = "${config.home.homeDirectory}/.secrets/vpn/WIREGUARD_ADDRESSES";
-        # owenr = config.users.users.maxlttr.name;
     };
 }
