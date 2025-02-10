3ba87ce0c5c74a584fc08f36e7bedb03f0cdd2a3
Maxime <maxime.lettier@protonmail.com>
2025-02-10 18:23:06 +0100

    wouhou

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -2,7 +2,7 @@
  imports = [
    ./docker.nix
    #./dockur_output.nix
-   ./headscale_output.nix
+   #./headscale_output.nix
    ./jellyfin_output.nix
    ./nextcloud_output.nix
    ./nginx_reverse_proxy_output.nix
