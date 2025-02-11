edc2750b884b2ff94050255cc5594ef99af4b990
Maxime <maxime.lettier@protonmail.com>
2025-02-11 08:31:49 +0100

    modified ssh settings

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -11,6 +11,9 @@
       AllowUsers = [ "${settings.username}" ]; # Allows all users by default. Can be [ "user1" "user2" ]
       PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
       KbdInteractiveAuthentication = false; #Specifies whether keyboard-interactive authentication is allowed
+      X11Forwarding = false;
+      UseDns = true;
+      startWhenNeeded = true;
     };
   }; 
 
