649904ff88787381813321614b1e7297dba69946
Maxime <maxime.lettier@protonmail.com>
2025-02-18 15:13:03 +0100

    test

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -3,7 +3,7 @@
    ./docker.nix
    #./dockur_output.nix
    #./headscale_output.nix
-   ./jellyfin_output.nix
+   #./jellyfin_output.nix
    ./nextcloud_output.nix
    ./nginx_reverse_proxy_output.nix
    ./pi-hole_output.nix
