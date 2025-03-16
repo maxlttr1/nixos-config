b8dc1451fba58653e78644a642b1df5dfff1af91
Maxime <maxime.lettier@protonmail.com>
2025-03-16 13:53:26 +0100

    edited fail2ban config

--- modules/fail2ban.nix
+++ modules/fail2ban.nix
@@ -1,1 +1,14 @@
-{services.fail2ban.enable = true;}
+{
+    services.fail2ban = {
+        enable = true;
+        maxretry = 5;
+        bantime = "1h";
+        bantime-increment = {
+            enable = true;
+            formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+            multipliers = "1 2 4 8 16 32 64";
+            maxtime = "168h"; # Do not ban for more than 1 week
+            overalljails = true; # Calculate the bantime based on all the violations
+        };
+    };
+}
