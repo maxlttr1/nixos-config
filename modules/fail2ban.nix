6fc6ea83a45b902002184d47088a2b7a3b7675ea
Maxime <maxime.lettier@protonmail.com>
2025-03-16 14:01:09 +0100

    mdif fail2ban

--- modules/fail2ban.nix
+++ modules/fail2ban.nix
@@ -5,8 +5,6 @@
         bantime = "1h";
         bantime-increment = {
             enable = true;
-            formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
-            multipliers = "1 2 4 8 16 32 64";
             maxtime = "168h"; # Do not ban for more than 1 week
             overalljails = true; # Calculate the bantime based on all the violations
         };
