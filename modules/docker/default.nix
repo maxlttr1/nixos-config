255d37688e73508adb5b368c81df2b6348c01c42
Maxime <maxime.lettier@protonmail.com>
2025-03-17 18:44:30 +0100

    changes to docker

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -1,15 +1,16 @@
 {
- imports = [
-   ./docker.nix
-   ./jellyfin/jellyfin.nix
-   #./nextcloud/nextcloud.nix
-   ./pihole/pihole.nix
-   ./portainer/portainer.nix
-   #./traefik/traefik.nix
-   ./uptime-kuma/uptime-kuma.nix
-   ./vpn/vpn.nix
-   ./watchtower/watchtower.nix
-   ./wg-easy/wg-easy.nix
+  imports = [
+    ./docker.nix
+    ./deluge/deluge.nix
+    ./gluetun/gluetun.nix
+    ./jellyfin/jellyfin.nix
+    #./nextcloud/nextcloud.nix
+    ./pihole/pihole.nix
+    ./portainer/portainer.nix
+    #./traefik/traefik.nix
+    ./uptime-kuma/uptime-kuma.nix
+    ./watchtower/watchtower.nix
+    ./wg-easy/wg-easy.nix
   ];
 
   # Run with compose2nix: compose2nix -inputs input.yml -output output.nix -runtime docker
