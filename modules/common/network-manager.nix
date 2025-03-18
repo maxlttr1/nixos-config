48cb7020e0857fa4a401663d85ef9e94615dad6d
Maxime <maxime.lettier@protonmail.com>
2025-03-18 11:36:02 +0100

    randomized mac adress for ethernet and wifi

--- modules/common/network-manager.nix
+++ modules/common/network-manager.nix
@@ -1,1 +1,8 @@
-{networking.networkmanager.enable = true;}
+{
+    networking.networkmanager = {
+        enable = true;
+        ethernet.macAddress = "random"; #  Generate a randomized value upon each connect
+        wifi.macAddress = "random";
+        wifi.scanRandMacAddress = true; # Whether to enable MAC address randomization of a Wi-Fi device during scanning.w
+    };
+}
