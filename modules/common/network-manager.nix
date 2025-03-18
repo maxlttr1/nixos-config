d1369e7d03c24ff088c9347486af011661f80382
Maxime <maxime.lettier@protonmail.com>
2025-03-18 11:37:24 +0100

    remoevd it was by default to true

--- modules/common/network-manager.nix
+++ modules/common/network-manager.nix
@@ -3,6 +3,5 @@
         enable = true;
         ethernet.macAddress = "random"; #  Generate a randomized value upon each connect
         wifi.macAddress = "random";
-        wifi.scanRandMacAddress = true; # Whether to enable MAC address randomization of a Wi-Fi device during scanning.w
     };
 }
