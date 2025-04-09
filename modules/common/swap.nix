f5d985b6847fc9d835fabe72c312f74905bb04af
Maxime <maxime.lettier@protonmail.com>
2025-04-09 18:16:54 +0200

    augmented swap file size to 32G

--- modules/common/swap.nix
+++ modules/common/swap.nix
@@ -2,7 +2,7 @@
   swapDevices = [{
     device = "/var/lib/swapfile";
     #label = "swapfile";
-    size = 16*1024; # Size is in megabytes
+    size = 32*1024; # Size is in megabytes
     priority = 2048; # Priority is a value between 0 and 32767. Higher numbers indicate higher priority. null lets the kernel choose a priority, which will show up as a negative value.
   }];
 
