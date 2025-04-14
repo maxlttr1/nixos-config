316d154bf245145c32c1cc31590698cab7d1dd60
Maxime <maxime.lettier@protonmail.com>
2025-04-14 17:11:39 +0200

    test for samba

---
+++ modules/samba.nix
@@ -0,0 +1,37 @@
+{ settings, ... }:
+
+{
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        #"map to guest" = "bad user";
+        "invalid users" = [ "root" ]; 
+      };
+      "share" = {
+        "path" = "~/Syncthing/samba";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = ${settings.username};
+        "force group" = "users";
+      };
+    };
+  };
+
+  # samba-wsdd service is used to advertise the shares to Windows hosts 
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+}
