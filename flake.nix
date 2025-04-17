49990001205250b97f33641dfdfe39776ea71a7c
Maxime <maxime.lettier@protonmail.com>
2025-04-17 18:02:10 +0200

    fallbakc to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Kakoukakou";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
