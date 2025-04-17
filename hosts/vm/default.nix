c48aca6c4ade7eeb0bcf4962964c5942fade067b
Maxime <maxime.lettier@protonmail.com>
2025-04-17 14:53:07 +0200

    typo

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -3,7 +3,7 @@
     ./hardware-configuration.nix
     ../../modules/common
     ../../modules/fish.nix
-    ../../impermanence.nix
+    ../../modules/impermanence.nix
     ../../modules/intel.nix
     ../../modules/ld.nix
     ../../modules/ssh.nix
