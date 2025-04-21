fc1352fc4928cee8374844ffaf2251920257c106
Maxime <maxime.lettier@protonmail.com>
2025-04-21 16:03:39 +0200

    test

--- modules/common/auto-update.nix
+++ modules/common/auto-update.nix
@@ -9,14 +9,14 @@ let
 
     echo "[$(date)] Starting upgrade..." >> $LOGFILE
 
-    if ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake github:maxlttr1/nixos-config#$(hostname) >> $LOGFILE 2>&1; then
+    if ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake github:maxlttr1/nixos-config >> $LOGFILE 2>&1; then
       echo "[$(date)] ✅ Upgrade succeeded." >> $LOGFILE
     else
       echo "[$(date)] ❌ Upgrade FAILED!" >> $LOGFILE
 
       ${pkgs.curl}/bin/curl -X POST $DISCORD_WEBHOOK_URL \
         -H "Content-Type: application/json" \
-        -d '{"content": "NixOS upgrade failed on host '"$(hostname)"'. Check the logs for details."}'
+        -d '{"content": "NixOS upgrade failed ❌. Check the logs for details."}'
     fi
   '';
 in
