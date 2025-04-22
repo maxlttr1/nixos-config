f22456c1df99c729d8a91119e1623ab842613f2e
Maxime <maxime.lettier@protonmail.com>
2025-04-22 15:54:23 +0200

    removed lockfile

--- modules/common/auto-update.nix
+++ modules/common/auto-update.nix
@@ -4,28 +4,23 @@ let
   script = ''
     set -e
     LOGFILE=/var/log/nixos-upgrade.log
-    LOCKFILE=/var/lock/nixos-upgrade.lock
     HOSTNAME=$(cat /etc/hostname)
     DISCORD_WEBHOOK_URL=$(cat /etc/discord-webhook.conf)
 
     echo "[$(date)] Starting upgrade..." >> $LOGFILE
 
-    if ${pkgs.util-linux}/bin/flock -n $LOCKFILE; then
-      if ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake github:maxlttr1/nixos-config#$HOSTNAME >> $LOGFILE 2>&1; then
-        echo "[$(date)] ✅ Upgrade succeeded." >> $LOGFILE
+    if ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake github:maxlttr1/nixos-config#$HOSTNAME >> $LOGFILE 2>&1; then
+      echo "[$(date)] ✅ Upgrade succeeded." >> $LOGFILE
 
-        ${pkgs.curl}/bin/curl -X POST $DISCORD_WEBHOOK_URL \
-          -H "Content-Type: application/json" \
-          -d "{\"content\": \"NixOS upgrade success on $HOSTNAME ✅\"}"
-      else
-        echo "[$(date)] ❌ Upgrade FAILED!" >> $LOGFILE
-
-        ${pkgs.curl}/bin/curl -X POST $DISCORD_WEBHOOK_URL \
-          -H "Content-Type: application/json" \
-          -d "{\"content\": \"NixOS upgrade failed on $HOSTNAME ❌. Check the logs for details.\"}"
-      fi
+      ${pkgs.curl}/bin/curl -X POST $DISCORD_WEBHOOK_URL \
+        -H "Content-Type: application/json" \
+        -d "{\"content\": \"NixOS upgrade success on $HOSTNAME ✅\"}"
     else
-      echo "[$(date)] Skipped: Another upgrade in progress" >> $LOGFILE
+      echo "[$(date)] ❌ Upgrade FAILED!" >> $LOGFILE
+
+      ${pkgs.curl}/bin/curl -X POST $DISCORD_WEBHOOK_URL \
+        -H "Content-Type: application/json" \
+        -d "{\"content\": \"NixOS upgrade failed on $HOSTNAME ❌. Check the logs for details.\"}"
     fi
   '';
 in
