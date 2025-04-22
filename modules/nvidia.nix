d228ce321c17cc869fc2724336eb10453bc751de
Maxime <maxime.lettier@protonmail.com>
2025-04-22 21:20:08 +0200

    typo

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -20,7 +20,7 @@
     open = false; #Since driver version 560, you also will need to decide whether to use the open-source or proprietary modules
 
     # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
+    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
