0d4cc9e6cca40dd62ff6a33f0203c25d0694cbc7
Maxime <maxime.lettier@protonmail.com>
2025-04-28 18:40:55 +0200

    typo

--- modules/common/auto-upgrade.nix
+++ modules/common/auto-upgrade.nix
@@ -8,7 +8,7 @@
       #"nixpkgs-main"
       "-L" # Show logs
     ];
-    dates = "Sun 02:00";  # Every Monday at 2 AM
+    dates = "Sun 02:00";  # Every Sunday at 2 AM
     randomizedDelaySec = "30min";
     persistent = true;
     operation = "switch"; # Or "boot"
