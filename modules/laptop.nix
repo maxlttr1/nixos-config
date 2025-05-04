a1c7e30ed2da3394dfa180240b1267c29ba2c677
Maxime <maxime.lettier@protonmail.com>
2025-05-04 16:08:12 +0200

    tlp settings

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -1,6 +1,5 @@
 {
   services.power-profiles-daemon.enable = false;
-  powerManagement.enable = true; # Stock NixOS power management tool which allows for managing hibernate and suspend states
   
   services.tlp = {
     enable = true;
