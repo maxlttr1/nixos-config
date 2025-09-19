c1a7d91caabbe3774ba91f6ec3c7147aafd2ee53
Maxime <maxime.lettier@protonmail.com>
2025-09-19 10:11:03 +0200

    doc for atd

--- modules/atd.nix
+++ modules/atd.nix
@@ -1,4 +1,4 @@
 {
-    services.atd.allowEveryone = true;
-    services.atd.enable = true;
+    #services.atd.allowEveryone = true;
+    services.atd.enable = true; # Daemon that runs jobs scheduled using the at or batch commands.
 }
