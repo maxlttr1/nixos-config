c8beb369ad892b38b19c4f3e52b74afa13ae1391
Maxime <maxime.lettier@protonmail.com>
2025-09-20 16:00:51 +0200

    added steam for asus

--- modules/pkgs-gaming.nix
+++ modules/pkgs-gaming.nix
@@ -14,18 +14,17 @@
 
   # Flatpaks
   services.flatpak.packages = [
-    "com.heroicgameslauncher.hgl"
-    "net.davidotek.pupgui2"
+    #"com.heroicgameslauncher.hgl"
+    #"net.davidotek.pupgui2"
     "com.github.Matoking.protontricks" # flatpak override --user --filesystem=/path/to/other/Steam/Library com.github.Matoking.protontricks
     "com.valvesoftware.Steam"
   ];
 
   environment.systemPackages = 
     (with pkgs; [
-      #lutris
-      #mangohud
     ])
     ++
     (with pkgs.alternative; [
+      steam-devices-udev-rules
     ]);
 }
