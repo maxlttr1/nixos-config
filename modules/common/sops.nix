4bf3e10aa95511349b02ed8de59fd7655074177d
Maxime <maxime.lettier@protonmail.com>
2025-12-01 10:28:19 +0100

    updated perm for ssh keys

--- modules/common/sops.nix
+++ modules/common/sops.nix
@@ -20,36 +20,43 @@
     sops.secrets."suaps.env" = {
         owner = "${settings.username}"; 
         path = "/home/${settings.username}/docker/suaps/suaps.env";
+        mode = "0600";
     };
 
     sops.secrets."nixos_ssh_setup.public" = {
         owner = "${settings.username}"; 
         path = "/home/${settings.username}/.ssh/nixos_ssh_setup.pub";
+        mode = "0640";
     };
 
     sops.secrets."nixos_ssh_setup.private" = {
         owner = "${settings.username}"; 
         path = "/home/${settings.username}/.ssh/nixos_ssh_setup";
+        mode = "0600";
     };
 
     sops.secrets."racknerd_ssh.public" = {
         owner = "${settings.username}"; 
         path = "/home/${settings.username}/.ssh/racknerd_ssh.pub";
+        mode = "0640";
     };
 
     sops.secrets."racknerd_ssh.private" = {
         owner = "${settings.username}"; 
         path = "/home/${settings.username}/.ssh/racknerd_ssh";
+        mode = "0600";
     };
 
     sops.secrets."gitlab-univ-nantes.public" = {
         owner = "${settings.username}"; 
         path = "/home/${settings.username}/.ssh/gitlab-univ-nantes.pub";
+        mode = "0640";
     };
     
     sops.secrets."gitlab-univ-nantes.private" = {
         owner = "${settings.username}"; 
         path = "/home/${settings.username}/.ssh/gitlab-univ-nantes";
+        mode = "0600";
     };
 
     sops.secrets."discord-webhook" = {
