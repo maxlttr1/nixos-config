2e2e167e55b70739a30626cc51b741df9f5e9ab2
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-01-07 11:28:12 +0100

    removed cookies librewolf

--- modules/homes/modules/librewolf.nix
+++ modules/homes/modules/librewolf.nix
@@ -15,8 +15,6 @@
           "network.trr.uri" = "	https://dns.quad9.net/dns-query";
           "network.trr.mode" = 3;
           "browser.startup.page" = 3; # Restore session
-          "privacy.clearOnShutdown.cookies" = false;
-          "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
         };
       };
     };
