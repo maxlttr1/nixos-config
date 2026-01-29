522860db91b1ba755b7d0de7c09c9c20cafed566
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-01-29 17:06:47 +0100

    changed upgrade frequency

--- nixosModules/common/autoUpgrade.nix
+++ nixosModules/common/autoUpgrade.nix
@@ -24,7 +24,7 @@ in
     autoUpgrade.enable = lib.mkEnableOption "Enable automatic NixOS upgrades";
     autoUpgrade.frequency = lib.mkOption {
       description = "AutoUpgrade frequency";
-      default = "daily";
+      default = "weekly";
       type = lib.types.enum [ "daily" "weekly" "monthly" "yearly" ];
     };
   };
