1d8bf22225004e3baa820522db56429760def541
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-01-30 19:23:03 +0100

    fix for esp size

--- hosts/test/disko.nix
+++ hosts/test/disko.nix
@@ -7,14 +7,15 @@
         content = {
           type = "gpt";
           partitions = {
-            esp = {
-              label = "esp";
-              size = "512m";
+            ESP = {
+              label = "ESP";
+              size = "512M";
               type = "ef00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
               };
             };
             root = {
