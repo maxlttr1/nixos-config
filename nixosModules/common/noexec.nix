e076d99e60b8e3e689391f6b8e93dca199f08dd4
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-02-01 11:34:35 +0100

    no exec readded

--- nixosModules/common/noexec.nix
+++ nixosModules/common/noexec.nix
@@ -5,64 +5,64 @@
     noexec.enable = lib.mkEnableOption "Enable noexec mount options for security";
   };
 
-  /*config.fileSystems = lib.mkMerge [
-     	(lib.mkIf config.noexec.enable {
-    		"/".options = lib.mkAfter [ 
-     			"nosuid"
-     			"nodev"
-     			"noatime" 
-    		];
-    		"/etc" = {
-     			device = "/etc";
-     			options = lib.mkAfter [
-    				"bind"
-    				"noexec"
-     			];
-    		};
-    		"/tmp" = {
-     			device = "/tmp";
-     			options = [
-    				"bind"
-    				"noexec"
-     			];
-    		};
-    		"/run/media" = {
-     			device = "/run/media";
-     			options = lib.mkAfter [
-    				"bind"
-    				"noexec"
-     			];
-    		};
-    		"/mnt" = {
-     			device = "/mnt";
-     			options = lib.mkAfter [
-    				"bind"
-    				"noexec"
-     			];
-    		};
-    		"/nix/store" = {
-     			device = "/mnt";
-     			options = lib.mkAfter [
-    				"bind"
-    				"noatime"
-     			];
-    		};
+  config.fileSystems = lib.mkMerge [
+    (lib.mkIf config.noexec.enable {
+      "/".options = lib.mkAfter [ 
+        "nosuid"
+        "nodev"
+        "noatime" 
+      ];
+      "/etc" = {
+        device = "/etc";
+        options = lib.mkAfter [
+          "bind"
+          "noexec"
+        ];
+      };
+      "/tmp" = {
+        device = "/tmp";
+        options = [
+          "bind"
+          "noexec"
+        ];
+      };
+      "/run/media" = {
+        device = "/run/media";
+        options = lib.mkAfter [
+          "bind"
+          "noexec"
+        ];
+      };
+      "/mnt" = {
+        device = "/mnt";
+        options = lib.mkAfter [
+          "bind"
+          "noexec"
+        ];
+      };
+      "/nix/store" = {
+        device = "/mnt";
+        options = lib.mkAfter [
+          "bind"
+          "noatime"
+        ];
+      };
     })
-     	(lib.mkIf config.users.enable {
-     	  "/home/${settings.username}/Public" = {
-    		device = "/home/${settings.username}/Public";
-    		options = lib.mkAfter [
-     			"bind" 
-     			"noexec" 
-    		];
-     	  };
-     	  "/home/${settings.username}/Downloads" = {
-    		device = "/home/${settings.username}/Downloads";
-    		options = lib.mkAfter [
-     			"bind" 
-     			"noexec"
-    		];
-     	  };
-     	})
-  ];*/
+    (lib.mkIf config.users.enable {
+      "/home/${settings.username}/Public" = {
+        device = "/home/${settings.username}/Public";
+        options = lib.mkAfter [
+          "bind" 
+          "noexec" 
+        ];
+      };
+      "/home/${settings.username}/Downloads" = {
+        device = "/home/${settings.username}/Downloads";
+        options = lib.mkAfter [
+          "bind" 
+          "noexec"
+        ];
+      };
+    })
+  ];
 }
