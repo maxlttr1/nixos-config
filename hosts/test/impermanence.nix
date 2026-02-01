cb2e9d6c09c340ad24b24331c397b30e55f6881d
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-02-01 11:35:28 +0100

    identation

--- hosts/test/impermanence.nix
+++ hosts/test/impermanence.nix
@@ -18,29 +18,29 @@
   config = lib.mkIf config.impermanence.enable {
     # Automatically remove roots that are older than 30 days
     boot.initrd.postResumeCommands = lib.mkAfter ''
-      			mkdir /btrfs_tmp
-      			mount ${config.impermanence.diskDevice} /btrfs_tmp
-      			if [[ -e /btrfs_tmp/root ]]; then
-      					mkdir -p /btrfs_tmp/old_roots
-      					timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-      					mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-      			fi
+			mkdir /btrfs_tmp
+			mount ${config.impermanence.diskDevice} /btrfs_tmp
+			if [[ -e /btrfs_tmp/root ]]; then
+					mkdir -p /btrfs_tmp/old_roots
+					timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+					mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+			fi
 
-      			delete_subvolume_recursively() {
-      					IFS=$'\n'
-      					for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-      							delete_subvolume_recursively "/btrfs_tmp/$i"
-      					done
-      					btrfs subvolume delete "$1"
-      			}
+			delete_subvolume_recursively() {
+					IFS=$'\n'
+					for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+							delete_subvolume_recursively "/btrfs_tmp/$i"
+					done
+					btrfs subvolume delete "$1"
+			}
 
-      			for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +${config.impermanence.retentionDays}); do
-      					delete_subvolume_recursively "$i"
-      			done
+			for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +${config.impermanence.retentionDays}); do
+					delete_subvolume_recursively "$i"
+			done
 
-      			btrfs subvolume create /btrfs_tmp/root
-      			umount /btrfs_tmp
-      		'';
+			btrfs subvolume create /btrfs_tmp/root
+			umount /btrfs_tmp
+		'';
 
     fileSystems."/persist".neededForBoot = true;
     environment.persistence."/persist" = {
