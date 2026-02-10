ee0544e274b2d705a4644b7a96ff336cc83ac79a
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-02-10 17:18:13 +0100

    fix for users

--- nixosModules/common/users.nix
+++ nixosModules/common/users.nix
@@ -11,7 +11,13 @@
         isNormalUser = true;
         createHome = true;
         extraGroups = [ "wheel" ];
-        hashedPasswordFile = "/home/${settings.username}/.config/sops-nix/secrets/passwd";
+        hashedPassword = "$y$j9T$cVaStq4nQnBE2Iz3wrZIM1$UdvZmv2wmvy93xnNl4h403DI30v8G8satfg4r0.vN9.";
+      };
+      "k7x9mq2v8nf4pj5w" = {
+        isNormalUser = true;
+        createHome = true;
+        extraGroups = [ "wheel" ];
+        hashedPassword = "$y$j9T$cVaStq4nQnBE2Iz3wrZIM1$UdvZmv2wmvy93xnNl4h403DI30v8G8satfg4r0.vN9.";
       };
       "root" = {
         hashedPassword = null; # Disable root login using a password 
