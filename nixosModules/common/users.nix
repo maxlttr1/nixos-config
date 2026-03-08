1a670a7fe54f2fd4b9d90649235ea9cd6e5790ce
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-03-08 19:27:16 +0100

    removed user maxlttr and forced uid of 1000

--- nixosModules/common/users.nix
+++ nixosModules/common/users.nix
@@ -12,12 +12,7 @@
         createHome = true;
         extraGroups = [ "wheel" ];
         hashedPassword = "$y$j9T$cVaStq4nQnBE2Iz3wrZIM1$UdvZmv2wmvy93xnNl4h403DI30v8G8satfg4r0.vN9.";
-      };
-      "maxlttr" = {
-        isNormalUser = true;
-        createHome = true;
-        extraGroups = [ "wheel" ];
-        hashedPassword = "$y$j9T$cVaStq4nQnBE2Iz3wrZIM1$UdvZmv2wmvy93xnNl4h403DI30v8G8satfg4r0.vN9.";
+        uid = 1000;
       };
       "root" = {
         hashedPassword = null; # Disable root login using a password 
