899c3a7ca0f09369624d5c30d75545b521880ed0
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-03-29 09:39:03 +0200

    added i2p content to jellyfin and added i2p browser settings

--- homeManagerModules/i2p-browser.nix
+++ homeManagerModules/i2p-browser.nix
@@ -15,15 +15,41 @@
 
     programs.librewolf = {
       enable = true;
-      profiles.default.settings = {
-        "network.proxy.type" = 1; # 1 = Manual proxy configuration, 5 = Use system proxy
-        "network.proxy.http" = "100.68.245.10";
-        "network.proxy.http_port" = 4444;
-        # "network.proxy.socks" = "127.0.0.1";
-        # "network.proxy.socks_port" = 4447;
-        "keyword.enabled" = false; # Don't search .i2p domains
-        "dom.security.https_only_mode" = false; # i2p doesn't use HTTPS
-      };
+      profiles.default = {
+        settings = {
+          "network.proxy.type" = 1; # 1 = Manual proxy configuration, 5 = Use system proxy
+          "network.proxy.http" = "100.68.245.10";
+          "network.proxy.http_port" = 4444;
+          "network.proxy.no_proxies_on" = "100.68.245.10/24,nexus-nexus,";
+          # "network.proxy.socks" = "127.0.0.1";
+          # "network.proxy.socks_port" = 4447;
+          "keyword.enabled" = false; # Don't search .i2p domains
+          "dom.security.https_only_mode" = false; # i2p doesn't use HTTPS
+        };
+				bookmarks = {
+					force = true;
+					settings = [
+						{
+							name = "i2p";
+							toolbar = true;
+							bookmarks = [
+								{
+								name = "router console";
+								url = "http://100.68.245.10:7657/";
+								}
+								{
+								name = "planet.i2p";
+								url = "http://planet.i2p/";
+								}
+								{
+								name = "postman.i2p";
+								url = "http://tracker2.postman.i2p";
+								}
+							];
+						}
+					];
+				};
+			};
     };
   };
 }
