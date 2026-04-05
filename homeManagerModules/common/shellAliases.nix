25bcb300551e35beeeb825fab6b785d34469950a
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-04-05 14:10:53 +0200

    created an aliases to have a ubuntu docker container in case of compatibility issues

--- homeManagerModules/common/shellAliases.nix
+++ homeManagerModules/common/shellAliases.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, settings, ... }:
 
 {
   options.custom.shellAliases.enable = lib.mkEnableOption "shell aliases";
@@ -12,6 +12,22 @@
       gl = "git log --oneline --decorate --all --graph";
       autoampdl = ''
         python src/main.py --url "https://www.youtube.com/watch?v=iSC4P1i9zmE&list=PLkF8ZEu4FB1kvxIhG9jLAEauSivb5JJGD&pp=sAgC" --download-archive ~/mountedDisk/syncthing/music/ids.txt --cookies ~/mountedDisk/syncthing/music/cookies.firefox-private.txt ~/mountedDisk/syncthing/music/AutoAmpDL/
+      '';
+      create-ubuntu = ''
+		docker build  \
+			--no-cache \
+			--build-arg UID=$(id -u) \
+			--build-arg USERNAME=$(id -un) \
+			-t custom-ubuntu \
+			$HOME/Documents/nixos-config/nixosModules/common/docker/inactive/ubuntu/
+	  '';
+      run-ubuntu = ''
+        docker run -it --rm \
+			-u $(id -u):$(id -g) \
+			-v "$HOME/Documents/:/home/$(id -un)/Documents:ro" \
+			-v "$HOME/mountedDisk/syncthing/:/home/$(id -un)/syncthing:ro" \
+			-w /home/$(id -un) \
+			custom-ubuntu /bin/bash
       '';
     };
   };
