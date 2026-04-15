fd489f7995ffe1fa7201d84b8dda6695e9b3bf10
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-04-15 16:24:13 +0200

    fix for vm boot

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,6 +1,12 @@
+{ lib, ... }:
+
 {
   custom.hostname = "vm";
 
   custom.impermanence.enable = true;
   custom.intel.enable = true;
+
+
+  custom.boot.enable = lib.mkForce true;
+  boot.loader.systemd-boot.enable = true;
 }
