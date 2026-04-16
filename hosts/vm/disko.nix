49b15a21272998564f8486a66995e7268240b518
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-04-16 12:57:10 +0200

    test to reintroduce encrypted btrfs

--- hosts/vm/disko.nix
+++ hosts/vm/disko.nix
@@ -7,16 +7,70 @@
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
+            ESP = {
+              label = "ESP";
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "umask=0077"
+                  "dmask=0077"
+                ];
+              };
             };
             root = {
               size = "100%";
+              label = "root";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "luks";
+                name = "crypted";
+                passwordFile = "/tmp/disk-encryption.key";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ 
+                    "-L"
+                    "nixos"
+                    "-f" # force creation
+                  ];
+                  postCreateHook = ''
+                    mount -t btrfs /dev/disk/by-label/nixos /mnt
+                    btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+                    umount /mnt
+                  '';
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "subvol=root"
+                        "compress=zstd"
+                        "noatime"
+                        "nosuid"
+                        "nodev"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "subvol=nix"
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = [
+                        "subvol=persist"
+                        "compress=zstd"
+                        "noatime"
+                        "nosuid"
+                        "nodev"
+                      ];
+                    };
+                  };
+                };
               };
             };
           };
