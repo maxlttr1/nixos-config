636bfffe31115094bd478b9e259741d73c148b70
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-04-16 10:45:37 +0200

    test for vm

--- hosts/vm/disko.nix
+++ hosts/vm/disko.nix
@@ -8,69 +8,21 @@
           type = "gpt";
           partitions = {
             ESP = {
-              label = "ESP";
-              size = "1G";
+              size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [
-                  "umask=0077"
-                  "dmask=0077"
-                ];
+                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
               size = "100%";
-              label = "root";
               content = {
-                type = "luks";
-                name = "crypted";
-                passwordFile = "/tmp/disk-encryption.key";
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ 
-                    "-L"
-                    "nixos"
-                    "-f" # force creation
-                  ];
-                  postCreateHook = ''
-                    mount -t btrfs /dev/disk/by-label/nixos /mnt
-                    btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
-                    umount /mnt
-                  '';
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [
-                        "subvol=root"
-                        "compress=zstd"
-                        "noatime"
-                        "nosuid"
-                        "nodev"
-                      ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "subvol=nix"
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
-                    "/persist" = {
-                      mountpoint = "/persist";
-                      mountOptions = [
-                        "subvol=persist"
-                        "compress=zstd"
-                        "noatime"
-                        "nosuid"
-                        "nodev"
-                      ];
-                    };
-                  };
-                };
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
