48c74cbeb7eec88734266693738e5068dc473b40
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-04-27 11:24:18 +0200

    tweaked kernel parameters for security

--- nixosModules/common/kernel.nix
+++ nixosModules/common/kernel.nix
@@ -14,6 +14,9 @@
 
     # Kernel sysctl settings (runtime parameters - can be changed without reboot)
     boot.kernel.sysctl = {
+      ###################################
+      ### KERNEL SECURITY & DEBUGGING ###
+      ###################################
       # Prevent coredumps from SUID programs (blocks info leaks from privileged processes)
       "fs.suid_dumpable" = 0;
       # Include process ID in coredump filename (prevents predictable filename exploits)
@@ -22,10 +25,12 @@
       "kernel.kexec_load_disabled" = 1;
       # Hide kernel pointers from unprivileged users (mitigates info disclosure attacks)
       "kernel.kptr_restrict" = 2;
+      # Users must have CAP_SYSLOG to use dmesg
+      "kernel.dmesg_restrict" = 1;
       # Panic on kernel oops - crash system when kernel detects internal bugs
       "kernel.panic_on_oops" = 1;
       # Restrict performance event monitoring to root only (prevents side-channel attacks)
-      "kernel.perf_event_paranoid" = 3;
+      "kernel.perf_event_paranoid" = 2;
       # SysRq exposes a lot of potentially dangerous debugging functionality to unprivileged users
       # 4 makes it so a user can only use the secure attention key. A value of 0 would disable completely
       "kernel.sysrq" = 0;
@@ -37,6 +42,16 @@
       "net.core.bpf_jit_harden" = 2;
       # Toggles whether or not unprivileged processes can create user namespaces	
       "kernel.unprivileged_userns_clone" = 1;
+      # Restrict userfaultfd to privileged processes only (mitigates kernel exploitation techniques and use-after-free flaws)
+      "vm.unprivileged_userfaultfd" = 0;
+      # Enable Address Space Layout Randomization (ASLR)
+      "kernel.randomize_va_space" = 2;
+      # Disable loading of kernel modules
+      # "kernel.modules_disabled" = 1;
+      
+      ############################
+      ### FILESYSTEM HARDENING ###
+      ############################
       # Only permit symlinks to be followed when outside of a world-writable sticky directory
       "fs.protected_symlinks" = 1;
       # Blocks hardlinking files you don’t own
@@ -45,6 +60,36 @@
       "fs.protected_fifos" = 2;
       # Restricts regular file creation in sticky dirs (2 = owner-only)
       "fs.protected_regular" = 2;
+
+      ##############################
+      ### NETWORKING (IPv4/IPv6) ###
+      ##############################
+      # Enable SYN cookies (prevents SYN flood attacks)
+      "net.ipv4.tcp_syncookies" = 1;
+      # Protects against TIME-WAIT assassination (TCP timestamp attacks)
+      "net.ipv4.tcp_rfc1337" = 1;
+      # Enable source validation of packets received (prevents IP spoofing)
+      # "net.ipv4.conf.default.rp_filter" = 1;
+      # "net.ipv4.conf.all.rp_filter" = 1;
+      # Disable ICMP redirects (prevents potential traffic redirection)
+      "net.ipv4.conf.all.accept_redirects" = 0;
+      "net.ipv4.conf.default.accept_redirects" = 0;
+      "net.ipv4.conf.all.secure_redirects" = 0;
+      "net.ipv4.conf.default.secure_redirects" = 0;
+      "net.ipv6.conf.all.accept_redirects" = 0;
+      "net.ipv6.conf.default.accept_redirects" = 0;
+      # Blocks packets trying to define their own route
+      "net.ipv6.conf.all.accept_source_route" = 0;
+      "net.ipv6.conf.default.accept_source_route" = 0;
+      # Disable sending ICMP redirects (normal for routers but unnecessary for end hosts)
+      "net.ipv4.conf.default.send_redirects" = 0;
+      "net.ipv4.conf.all.send_redirects" = 0;
+      "net.ipv6.conf.all.send_redirects" = 0;
+      "net.ipv6.conf.default.send_redirects" = 0;
+      # Ignore all ICMP Echo Requests (Blocks ping requests, can mitigate some network scanning/reconnaissance)
+      # "net.ipv4.icmp_echo_ignore_all" = 1;
+      # Prevent bogus ICMP errors from filling up logs.
+      "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
     };
 
     # Kernel boot parameters (passed at boot time, cannot be changed without reboot)
@@ -55,6 +100,13 @@
       "module.sig_enforce=1"
       # Disables vsyscalls, they've been replaced with vDSO
       "vsyscall=none"
+      # Enables randomization in the kernel’s page allocator
+      "page_alloc.shuffle=1"
+      # Enables zeroing of memory during allocation and free time (helps mitigate use-after-free vulnerabilaties)
+      "init_on_alloc=1"
+      "init_on_free=1"
+      # Makes it harder to influence slab cache layout
+      "slab_nomerge"
     ];
 
     security = {
