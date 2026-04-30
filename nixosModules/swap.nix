603f6806fbe10c4b5ff295624629dccb32e7c210
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-04-30 14:50:08 +0200

    removed swap priority

--- nixosModules/swap.nix
+++ nixosModules/swap.nix
@@ -28,7 +28,7 @@
         #label = "swapfile";
         # randomEncryption.enable = true; 
         size = config.custom.swap.swapFile.sizeGiB * 1024; # Size is in megabytes
-        priority = 2048; # Priority is a value between 0 and 32767. Higher numbers indicate higher priority. null lets the kernel choose a priority, which will show up as a negative value.
+        # priority = 2048; # Priority is a value between 0 and 32767. Higher numbers indicate higher priority. null lets the kernel choose a priority, which will show up as a negative value.
       }];
     })
     (lib.mkIf config.custom.swap.zramSwap.enable {
