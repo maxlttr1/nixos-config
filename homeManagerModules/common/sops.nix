ba2cc24743c8559bfedaa665442539a21d009ae7
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-05-06 13:42:15 +0200

    added github keys

--- homeManagerModules/common/sops.nix
+++ homeManagerModules/common/sops.nix
@@ -1,4 +1,4 @@
-{ config, lib, settings, ... }:
+{ config, lib, ... }:
 
 {
   options.custom.sops.enable = lib.mkEnableOption "sops-nix secrets management";
@@ -12,17 +12,45 @@
       age.keyFile = "/etc/sops/age/keys.txt";
 
       secrets = {
-        "github-token" = { mode = "0600"; };
-        "vpn.env" = { mode = "0600"; };
-        "suaps.env" = { mode = "0600"; };
-        "nixos_ssh_setup.public" = { mode = "0640"; };
-        "nixos_ssh_setup.private" = { mode = "0600"; };
-        "racknerd_ip" = { mode = "0600"; };
-        "racknerd_ssh.public" = { mode = "0640"; };
-        "racknerd_ssh.private" = { mode = "0600"; };
-        "gitlab-univ-nantes.public" = { mode = "0640"; };
-        "gitlab-univ-nantes.private" = { mode = "0600"; };
-        "discord-webhook" = { mode = "0600"; };
+        "github-token" = {
+          mode = "0600";
+        };
+        "github.public" = {
+          mode = "0640";
+        };
+        "github.private" = {
+          mode = "0600";
+        };
+        "vpn.env" = {
+          mode = "0600";
+        };
+        "suaps.env" = {
+          mode = "0600";
+        };
+        "nixos_ssh_setup.public" = {
+          mode = "0640";
+        };
+        "nixos_ssh_setup.private" = {
+          mode = "0600";
+        };
+        "racknerd_ip" = {
+          mode = "0600";
+        };
+        "racknerd_ssh.public" = {
+          mode = "0640";
+        };
+        "racknerd_ssh.private" = {
+          mode = "0600";
+        };
+        "gitlab-univ-nantes.public" = {
+          mode = "0640";
+        };
+        "gitlab-univ-nantes.private" = {
+          mode = "0600";
+        };
+        "discord-webhook" = {
+          mode = "0600";
+        };
       };
     };
   };
