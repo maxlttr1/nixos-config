e5f63b78e590b42e6bebe1bdc13ea4189d2274d6
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-06-04 22:05:42 +0200

    discard once for swap

--- nixosModules/swap.nix
+++ nixosModules/swap.nix
@@ -30,6 +30,7 @@
           # randomEncryption.enable = true;
           size = config.custom.swap.swapFile.sizeGiB * 1024; # Size is in megabytes
           # priority = 2048; # Priority is a value between 0 and 32767. Higher numbers indicate higher priority. null lets the kernel choose a priority, which will show up as a negative value.
+          options = [ "discard=once" ]; # Runs discard exactly once when the swap is enabled
         }
       ];
     })
