a5704b9e6aecf404e9a84df544b8a24d90ae1b2b
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-07-06 17:57:38 +0200

    added a disko config

---
+++ disko/encrypted-impermance-btrfs.nix
@@ -0,0 +1,77 @@
+{ settings, ... }:
+
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "${settings.diskID}";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              label = "ESP";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "umask=0077"
+                  "dmask=0077"
+                ];
+              };
+            };
+            root = {
+              size = "100%";
+              label = "root";
+              content = {
+                type = "luks";
+                name = "crypted";
+                passwordFile = "/tmp/disk-encryption.key";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [
+                    "-L"
+                    "nixos"
+                    "-f" # force creation
+                  ];
+                  postCreateHook = ''
+                    mkdir -p /mnt
+                    mount -t btrfs /dev/mapper/crypted /mnt
+                    btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+                    umount /mnt
+                  '';
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "subvol=root"
+                        "compress=zstd"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "subvol=nix"
+                        "compress=zstd"
+                      ];
+                    };
+                    "/persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = [
+                        "subvol=persist"
+                        "compress=zstd"
+                      ];
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
