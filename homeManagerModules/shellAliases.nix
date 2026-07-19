e256cd35305fb9c234e4de91d8d870a177c09795
Maxime LETTIER <maxime.lettier@protonmail.com>
2026-07-19 16:32:01 +0200

    added shells alaises for yt-dlp and music normalization

--- homeManagerModules/shellAliases.nix
+++ homeManagerModules/shellAliases.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   settings,
   ...
 }:
@@ -34,6 +35,19 @@
                 custom-ubuntu /bin/bash
       '';
       ssh-vps = "ssh root@$(cat /home/${settings.username}/.config/sops-nix/secrets/racknerd_ip) -i /home/${settings.username}/.config/sops-nix/secrets/racknerd_ssh.private";
+      music-download = ''
+        ${pkgs.yt-dlp}/bin/yt-dlp \
+          "https://www.youtube.com/watch?v=AQ6iA-0CRL8&list=PLkF8ZEu4FB1kvxIhG9jLAEauSivb5JJGD" \
+          -f "bestaudio" \
+          --remux-video "opus" \
+          --embed-metadata --embed-thumbnail \
+          -o "$HOME/mountedDisk/syncthing/music/New/%(title)s [%(id)s].%(ext)s" \
+          --download-archive "$HOME/mountedDisk/syncthing/music/downloaded.txt" \
+          --cookies  "$HOME/mountedDisk/syncthing/music/cookies.firefox-private.txt"
+      '';
+      music-normalize = ''
+        ${pkgs.rsgain}/bin/rsgain easy -p no_album --skip-existing "$HOME/mountedDisk/syncthing/music/"
+      '';
     };
   };
 }
