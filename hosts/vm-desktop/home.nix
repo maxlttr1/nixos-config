8313c133d3ab832646b82d7e51cc3583ea0fab9d
maxime <maxime.lettier@ninjaone.com>
2026-08-05 14:05:50 +0200

    typo

--- hosts/vm-desktop/home.nix
+++ hosts/vm-desktop/home.nix
@@ -2,7 +2,7 @@
   custom.firefox.enable = true;
   custom.fish.enable = true;
   custom.git.enable = true;
-  custim.plasmaManager.enable = true;
+  custom.plasmaManager.enable = true;
   custom.shellAliases.enable = true;
   custom.yakuake.enable = true;
 }
