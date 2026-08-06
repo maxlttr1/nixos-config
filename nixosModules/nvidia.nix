91fb589fc85ff4052d4226039c2e0cfce53edfcd
maxime <maxime.lettier@ninjaone.com>
2026-08-06 11:47:17 +0200

    changes to nvidia drivers

--- nixosModules/nvidia.nix
+++ nixosModules/nvidia.nix
@@ -18,8 +18,7 @@
     services.xserver.videoDrivers = [ "nvidia" ];
 
     hardware.nvidia = {
-      # Modesetting is required.
-      modesetting.enable = true;
+      modesetting.enable = true; # Wayland requirement
 
       # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
       # Enable this if you have graphical corruption issues or application crashes after waking
@@ -27,14 +26,9 @@
       # of just the bare essentials.
       #powerManagement.enable = false;
 
-      open = false; # Since driver version 560, you also will need to decide whether to use the open-source or proprietary modules
+      open = false;
 
-      # Enable the Nvidia settings menu,
-      # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
-
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      nvidiaSettings = true; # Enable the Nvidia settings menu, accessible via `nvidia-settings`.
     };
 
     # Thermald proactively prevents overheating on Intel CPUs and works well with other tools
